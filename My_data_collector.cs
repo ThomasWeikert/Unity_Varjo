16bfe11a9dbd9e861493bf1e5aae37938196f9d8
tommvi <61557916+ThomasWeikert@users.noreply.github.com>
2021-02-18 15:53:11 +0200

    Add files via upload

---
+++ My_data_collector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class My_data_collector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("hi");
+        Vector3 headset_pos = this.transform.localPosition;
+        Quaternion headset_orient = this.transform.localRotation;
+        print(headset_pos + ";" + headset_orient);
+        addRecord(headset_pos, headset_orient, "cake.txt");
+
+    }
+
+
+    public static void addRecord(Vector3 headset_pos, Quaternion headset_orient, string filepath)
+
+    {
+        try
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+            {
+                file.WriteLine(headset_pos + "," + headset_orient + "," + GetTimeStamp());
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("This program did an oopsie : ", ex);
+        }
+    }
+
+
+    static string GetTimeStamp()
+    {
+        return System.DateTime.Now.ToString();
+    }
+
+
+
+}
+
+
+
